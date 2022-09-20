6ff8f16a5c843aced7687ddca96b5cc768b232b5
Ricardo <ricardojustoc@gmail.com>
2022-09-20 18:14:31 -0500

    submit de formulario

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
 const valor1 = document.querySelector('#valor1');
 const valor2 = document.querySelector('#valor2');
 const pResult = document.getElementById('result');
 const btnCalcular = document.querySelector('#btnCalcular');
 
-btnCalcular.addEventListener('click', onClick);
-function onClick() {
+form.addEventListener('submit', sumarValues);
+function sumarValues(e) {
+    e.preventDefault();
+    console.log(e);
     const suma = valor1.value +valor2.value;
     pResult.innerText = 'Result: '+suma;
     
 }
+
