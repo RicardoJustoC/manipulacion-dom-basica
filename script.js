067528b46d5bbf304acc2345c08ec4b3498f97ce
Ricardo <ricardojustoc@gmail.com>
2022-09-20 17:50:03 -0500

    addeventlistener a button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const h1 = document.querySelector("h1");
 const valor1 = document.querySelector('#valor1');
 const valor2 = document.querySelector('#valor2');
-
+const pResult = document.getElementById('result');
 const btnCalcular = document.querySelector('#btnCalcular');
-pResult = document.getElementById('result');
+
+btnCalcular.addEventListener('click', onClick);
 function onClick() {
     const suma = valor1.value +valor2.value;
     pResult.innerText = 'Result: '+suma;
